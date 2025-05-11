d856894a5f6f4308c5cfcad05832c78f85981b10
Sakshi choudhary <126265722+Choudharysakshi0605@users.noreply.github.com>
2025-05-11 17:39:56 +0530

    Update parking.py

--- parking.py
+++ parking.py
@@ -1,4 +1,8 @@
 import mysql.connector
-con=mysql.connector.connect(host='localhost',password='sakshi2002',user='root')
+con=mysql.connector.connect(host='localhost',password='sakshi2002',user='root',database="parking")
 if con.is_connected():
    print("connection established")
+mycursor=con.cursor()   
+mycursor.execute("select * from parking_entries")
+for i in mycursor:
+   print(i)
